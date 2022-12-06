fff16de36dabf14850b2ad0134d8927f13077eee
Owen Watkins <zkac249@live.rhul.ac.uk>
2022-12-06 21:21:05 +0000

    Deleted file that was meant to be pushed to sandpit. Modified variable List of entries to be public.

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public class Stack {
   int size = 0;
-  private List<Entry> entries = new ArrayList<>();
+  public List<Entry> entries = new ArrayList<>();
 
   /**
    * Creates empty Stack object.
